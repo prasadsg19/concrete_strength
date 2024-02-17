61045afe8c3d46ef7b637318d0a6afe099669e84
prasadsg19 <ghorpadeprasad19@gmail.com>
2024-02-17 21:42:21 +0530

    APi File is added

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask , request , jsonify , render_template
+from utils import get_prediction
+
+
+
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def concrete_strength():
+    data=request.form
+
+    cement=eval(data['cement'])
+    Blast_furnace_slag=eval(data['blast_furnace_slag'])
+    Fly_Ash=eval(data['fly_ash'])
+    water=eval(data['water'])
+    Superplasticizer=eval(data['superplasticizer'])
+    Coarse_Aggregate=eval(data['coarse_aggregate'])
+    Fine_Aggregate=eval(data['fine_aggregate'])
+    Age=eval(data['age'])
+
+    
+    result=get_prediction(cement, Blast_furnace_slag, Fly_Ash, water,
+                          Superplasticizer, Coarse_Aggregate, Fine_Aggregate,Age)
+    
+    return render_template('index.html',prediction_text=f'Concrete compressive strength is {result} Mpa')
+        
+
+    
+
+if __name__ == '__main__' :
+    app.run(debug=False)
+    
+
+
